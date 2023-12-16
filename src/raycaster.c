a523494da28025e63d17a62278c3b924dba426c9
joppe <jboeve009@gmail.com>
2023-12-16 01:02:46 +0100

    Can cast and draw rays in minimap, but for some reason not yet in the Y-axis.

--- src/raycaster.c
+++ src/raycaster.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <jboeve@student.codam.nl>            +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/12/15 15:20:09 by jboeve        #+#    #+#                 */
-/*   Updated: 2023/12/15 19:32:00 by joppe         ########   odam.nl         */
+/*   Updated: 2023/12/16 01:02:12 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,44 +20,42 @@
 
 
 
-t_ray raycaster_cast(t_meta *m, t_vec2f start, t_vec2f direction, t_ray_hit_check *hit)
+t_ray raycaster_cast(t_meta *m, t_vec2f start, t_vec2f direction, t_ray_hit_check *hit_check)
 {
-	t_ray r;
-	ft_bzero(&r, sizeof(t_ray));
-	t_vec2f		delta_distance;
+	t_ray		r;
 	t_vec2i		step;
 	t_vec2i		map_pos;
 	t_vec2f		side_distance;
+	t_vec2f		delta_distance;
 
+	ft_bzero(&r, sizeof(t_ray));
 
+	r.start = start;
 	delta_distance[VEC_X] = (direction[VEC_X] == 0) ? 1e30 : fabs(1 / direction[VEC_X]);
 	delta_distance[VEC_Y] = (direction[VEC_Y] == 0) ? 1e30 : fabs(1 / direction[VEC_Y]);
 
-	t_vec2f p_pos = start;
-	map_pos = vec2f_to_vec2i(p_pos);
-
+	map_pos = vec2f_to_vec2i(start);
 	if (direction[VEC_X] < 0)
 	{
 		step[VEC_X] = -1;
-		side_distance[VEC_X] = (p_pos[VEC_X] - map_pos[VEC_X]) * delta_distance[VEC_X];
+		side_distance[VEC_X] = (start[VEC_X] - map_pos[VEC_X]) * delta_distance[VEC_X];
 	}
 	else
 	{
 		step[VEC_X] = 1;
-		side_distance[VEC_X] = (map_pos[VEC_X] + 1.0f - p_pos[VEC_X]) * delta_distance[VEC_X];
+		side_distance[VEC_X] = (map_pos[VEC_X] + 1.0f - start[VEC_X]) * delta_distance[VEC_X];
 	}
 	if (direction[VEC_Y] < 0)
 	{
 		step[VEC_Y] = -1;
-		side_distance[VEC_Y] = (p_pos[VEC_Y] - map_pos[VEC_Y]) * delta_distance[VEC_Y];
+		side_distance[VEC_Y] = (start[VEC_Y] - map_pos[VEC_Y]) * delta_distance[VEC_Y];
 	}
 	else
 	{
 		step[VEC_Y] = 1;
-		side_distance[VEC_Y] = (map_pos[VEC_Y] + 1.0f - p_pos[VEC_Y]) * delta_distance[VEC_Y];
+		side_distance[VEC_Y] = (map_pos[VEC_Y] + 1.0f - start[VEC_Y]) * delta_distance[VEC_Y];
 	}
 
-	r.hit = false;
 	while (!r.hit)
 	{
 		if (side_distance[VEC_X] < side_distance[VEC_Y])	
@@ -72,7 +70,7 @@ t_ray raycaster_cast(t_meta *m, t_vec2f start, t_vec2f direction, t_ray_hit_chec
 			map_pos[VEC_Y] += step[VEC_Y];
 			r.hit_side = HIT_EW;
 		}
-		r.hit = *hit;
+		r.hit = hit_check(&m->map, map_pos[VEC_X], map_pos[VEC_Y]);
 	}
 
 	if(r.hit_side == HIT_EW)
@@ -82,6 +80,6 @@ t_ray raycaster_cast(t_meta *m, t_vec2f start, t_vec2f direction, t_ray_hit_chec
 	else
 		r.len = (side_distance[VEC_X] - delta_distance[VEC_X]);
 
-	r.end = direction * (t_vec2f) {r.len, r.len} + vec2i_to_vec2f(map_pos);
+	r.end = r.start + ((t_vec2f) {r.len, r.len} * direction);
 	return r;
 }
