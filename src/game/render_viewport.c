4180a5f3d5b2613f8ed38cd2a529b11751cfb77a
joppe <joppe@pop-os.localdomain>
2024-01-17 23:58:06 +0100

    Found bug with the center of the textures, weird artifacting..

--- src/game/render_viewport.c
+++ src/game/render_viewport.c
@@ -6,7 +6,7 @@
 /*   By: yzaim <marvin@42.fr>                         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/08 15:28:08 by yzaim         #+#    #+#                 */
-/*   Updated: 2024/01/17 16:29:48 by jboeve        ########   odam.nl         */
+/*   Updated: 2024/01/17 23:57:43 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,17 @@
 #include <stdio.h>
 #include <math.h>
 
-static t_vec2d	calculate_line_points(uint32_t h, double line_height)
-{
-	//calculate lowest and highest pixel to fill in current stripe
-	uint32_t	start;
-	uint32_t	end;
-
-	start = -line_height / 2 + ((double)h) / 2;
-	end = line_height / 2 + ((double)h) / 2;
-	if (end >= h)
-		end = h - 1;
-	return ((t_vec2d) {start, end});
-}
-
 mlx_texture_t	*get_texture(t_side side, t_attr attributes);
 
 static void	draw_column(t_meta *meta, t_vec2i line, t_ray *ray, uint32_t col, uint32_t h)
 {
+	int32_t			y;
 	int32_t			color;
 	mlx_texture_t	*texture;
 
 	texture = get_texture(ray->hit_side, meta->attributes);
 
-	ray->texture_point.x = (int)(ray->wall_x * (double)texture->width);
+	ray->texture_point.x = (int)(ray->wall_x * texture->width);
 	if ((ray->hit_side == SIDE_N || ray->hit_side == SIDE_S) && ray->direction.x > 0)
 		ray->texture_point.x = texture->width - ray->texture_point.x - 1;
 	if ((ray->hit_side == SIDE_E || ray->hit_side == SIDE_W) && ray->direction.y < 0)
@@ -47,13 +35,6 @@ static void	draw_column(t_meta *meta, t_vec2i line, t_ray *ray, uint32_t col, ui
 	ray->step = 1.0 * texture->height / ray->line_height;
 	ray->texture_position = (ray->line_point.x + (ray->line_height - h) / 2) * ray->step;
 
-
-	// printf("line_height [%lf]\n", ray->line_height);
-	// printf("step [%lf]\n", ray->step);
-	// printf("texture_point [%d]\n", ray->texture_point.x);
-	// printf("texture_position [%lf]\n", ray->texture_position);
-
-	int32_t y;
 	y = 0;
 	while (y < (int32_t) h)
 	{
@@ -70,16 +51,6 @@ static void	draw_column(t_meta *meta, t_vec2i line, t_ray *ray, uint32_t col, ui
 		mlx_put_pixel(meta->image, col, y, color);
 		y++;
 	}
-
-}
-double	calculate_line_height(double ray_length, uint32_t h)
-{
-	double		line_height;
-
-	line_height = (int)(h / ray_length);
-	if (line_height > h)
-		line_height = h;
-	return (line_height);
 }
 
 void render_viewport(mlx_image_t *image, t_player *p)
@@ -87,9 +58,6 @@ void render_viewport(mlx_image_t *image, t_player *p)
 	uint32_t col = 0;
 	while(col < image->width)
 	{
-		// p->rays[i].line_height = calculate_line_height(p->rays[i].length, image->height);
-		// t_vec2d line = calculate_line_points(image->height, p->rays[i].line_height);
-		
 		draw_column(p->meta, p->rays[col].line_point, &p->rays[col], col, image->height);
 		col++;
 	}
