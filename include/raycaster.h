4d7bb0e718c0fc67e26bf85262b72347d8c79eca
joppe <joppe@pop-os.localdomain>
2024-03-03 18:53:35 +0100

    Revert "Norm"
    This reverts commit 6f81325d4e5bdd52c9fa659c1081394648f757c9.

--- include/raycaster.h
+++ include/raycaster.h
@@ -6,23 +6,27 @@
 /*   By: jboeve <jboeve@student.codam.nl>            +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2024/03/01 12:58:39 by jboeve        #+#    #+#                 */
-/*   Updated: 2024/03/03 18:51:37 by joppe         ########   odam.nl         */
+/*   Updated: 2024/03/01 13:10:03 by jboeve        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RAYCASTER_H
-# define RAYCASTER_H
+#define RAYCASTER_H
 
-# include "vector.h"
-# include <math.h>
-# include <stdbool.h>
-# include "meta.h"
+#include "vector.h"
+#include <math.h>
+#include <stdbool.h>
+#include "meta.h"
 
 t_vec2d	calculate_delta_dist(t_vec2d ray_direction);
 t_vec2d	calculate_side_dist(t_vec2d ray_direction, \
-t_vec2d	player_pos, t_vec2i map_pos, t_vec2d delta_dist);
+		t_vec2d player_pos, t_vec2i map_pos, t_vec2d delta_dist);
 t_vec2i	calculate_step_size(t_vec2d ray_direction);
 double	calculate_ray_length(t_side hit_side, \
 		t_vec2d side_dist, t_vec2d delta_dist);
 
-#endif
+
+
+
+#endif // !RAYCASTER_H
+
--- include/render_minimap.h
+++ include/render_minimap.h
@@ -6,20 +6,20 @@
 /*   By: joppe <jboeve@student.codam.nl>             +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2024/03/01 19:29:27 by joppe         #+#    #+#                 */
-/*   Updated: 2024/03/03 18:52:00 by joppe         ########   odam.nl         */
+/*   Updated: 2024/03/01 19:36:18 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RENDER_MINIMAP_H
-# define RENDER_MINIMAP_H
+#define RENDER_MINIMAP_H
 
-# include "meta.h"
+#include "meta.h"
 
-t_rgba		get_cell_color(t_cell_type cell);
-void		render_clear_bg(mlx_image_t *image, uint32_t c);
+t_rgba	get_cell_color(t_cell_type cell);
+void	render_clear_bg(mlx_image_t *image, uint32_t c);
 t_cell_type	minimap_ray_len(const void *p, uint32_t x, uint32_t y);
-void		draw_cell(mlx_image_t *image, t_cell_type cell, const uint32_t x, \
-			const uint32_t y);
-void		render_border(mlx_image_t *image, uint32_t c);
+void	draw_cell(mlx_image_t *image, t_cell_type cell, const uint32_t x, const uint32_t y);
+void	render_border(mlx_image_t *image, uint32_t c);
+
 
 #endif
