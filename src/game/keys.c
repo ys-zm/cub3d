ac973303c4e50f15c0a2421f8cc020e2d4c6eb79
Yesim Zaim <yzaim@f1r6s16.codam.nl>
2024-03-04 12:32:00 +0100

    added arrow keys to look around

--- src/game/keys.c
+++ src/game/keys.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   keys.c                                            :+:    :+:             */
+/*   keys.c                                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: yzaim <marvin@42.fr>                         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/08 15:27:07 by yzaim         #+#    #+#                 */
-/*   Updated: 2024/03/01 16:43:55 by jboeve        ########   odam.nl         */
+/*   Updated: 2024/03/04 12:31:30 by yzaim         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,9 +80,9 @@ void	keys_handle(t_meta *meta, double delta_time)
 		rotate_speed /= PLAYER_ROTATE_MODIFIER;
 	if (mlx_is_key_down(meta->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(meta->mlx);
-	if (mlx_is_key_down(meta->mlx, MLX_KEY_Q))
+	if (mlx_is_key_down(meta->mlx, MLX_KEY_Q) || mlx_is_key_down(meta->mlx, MLX_KEY_LEFT))
 		player_turn(p, -rotate_speed);
-	if (mlx_is_key_down(meta->mlx, MLX_KEY_E))
+	if (mlx_is_key_down(meta->mlx, MLX_KEY_E) || mlx_is_key_down(meta->mlx, MLX_KEY_RIGHT))
 		player_turn(p, rotate_speed);
 	if (key_pressed(meta, MLX_KEY_F))
 		player_interact(p);
