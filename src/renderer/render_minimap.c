e5562c063d3f2cbbe3f5ce09f3bd04e002bbdbfe
yesimzaim <yesimzaim@gmail.com>
2024-06-05 13:23:00 +0200

    new debug comment

--- src/renderer/render_minimap.c
+++ src/renderer/render_minimap.c
@@ -6,7 +6,7 @@
 /*   By: yzaim <marvin@42.fr>                         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/08 15:27:53 by yzaim         #+#    #+#                 */
-/*   Updated: 2024/06/05 13:15:12 by yesimzaim     ########   odam.nl         */
+/*   Updated: 2024/06/05 13:22:42 by yesimzaim     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,12 @@ static void	draw_tiles(mlx_image_t *image, const t_vec2i image_center, const t_p
 {
 	const uint32_t	pp_center_x = image_center.x - (p->position.x * p->meta->minimap.tile_size);
 	const uint32_t	pp_center_y = image_center.y - (p->position.y * p->meta->minimap.tile_size);
+
+	printf("Player Pos: (%d, %d)\n", pp_center_x, pp_center_y);
 	size_t			loop[2];
 	int32_t			cell_pos[2];
 	const size_t	size = p->meta->minimap.tile_size;
+
 
 	loop[0] = 0;
 	while (loop[0] < map->width)
@@ -62,13 +65,13 @@ static void	render_minimap_level(mlx_image_t *image, const t_map *map, const t_p
 	{
 		r = &p->hrays[i];
 		draw_line(image, image_center, vec2d_to_vec2i(vec2d_add((t_vec2d){image_center.x, image_center.y}, vec2d_scalar_product(r->direction, r->length * size))), (t_rgba){0x999999FF});
-		if ( i == WINDOW_WIDTH / 2)
-		{
-			// Debugging output
-        	printf("Ray %zu: Start (%d, %d) Direction (%.3f, %.3f) Length %.3f Size %zu\n",
-               i, image_center.x, image_center.y, r->direction.x, r->direction.y, r->length, size);
-
-		}
+		// if ( i == WINDOW_WIDTH / 2)
+		// {
+		// 	// Debugging output
+  //       	printf("Ray %zu: Start (%d, %d) Direction (%.3f, %.3f) Length %.3f Size %zu\n",
+  //              i, image_center.x, image_center.y, r->direction.x, r->direction.y, r->length, size);
+		//
+		// }
 		i++;
 	}
 	draw_rect(image, pos, (t_vec2u){MINIMAP_PLAYER_SIZE, MINIMAP_PLAYER_SIZE}, MINIMAP_COLOR_PLAYER);
